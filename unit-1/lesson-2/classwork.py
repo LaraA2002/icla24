ef5fbdb611103f630f9dce81f686d316dfaeda57
LaraA02 <Lara02agrawal@gmail.com>
2024-09-06 21:20:38 -0400

    git push-up attempt 1

---
+++ unit-1/lesson-2/classwork.py
@@ -0,0 +1,16 @@
+score = 28
+# comment
+print(score)
+
+count = 0
+print(count)
+count = count + 1
+print(count)
+
+names = ["badah", "beimnet", "kirsten", "cate"]
+numberofnames = len(names)
+print(numberofnames)
+print(names[0])
+
+for person in names:
+    print(person)
